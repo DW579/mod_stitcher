7ad52aa9381d4ffec54f133000d144fabf862f32
Dustin Wurtz <dtwurtz@gmail.com>
2021-08-28 12:04:16 -0700

    Mod dropdown implemented

--- src/App.js
+++ src/App.js
@@ -4,39 +4,73 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
     state = {
-        client_selected: "ATT",
         client: "",
         shell: "",
         disableClient: false,
         disableShell: true,
+        disableMod: true,
         data: {
-            ATT: {
+            "AT&T": {
                 name: "AT&T",
-                shells: ["CLM", "CrossSell_Template", "DirectTVStream"]
+                shells: ["CLM", "CrossSell_Template", "DirectTVStream"],
+                CLM: {
+                    mods: ["image", "text"],
+                    image: {
+                        html: "<img />",
+                    },
+                    text: {
+                        html: "<p></p>",
+                    },
+                },
+                CrossSell_Template: {
+                    image: {
+                        html: "<img />",
+                    },
+                    text: {
+                        html: "<p></p>",
+                    },
+                },
+                DirectTVStream: {
+                    image: {
+                        html: "<img />",
+                    },
+                    text: {
+                        html: "<p></p>",
+                    },
+                },
             },
-            HD: {
+            "H-D": {
                 name: "Harley Davidson",
-                shells: ["eComm", "Insurance", "Museum_Rublings", "New_Brand", "Visa", "PanAm"]
-            }
-        }
-    }
+                shells: [
+                    "eComm",
+                    "Insurance",
+                    "Museum_Rublings",
+                    "New_Brand",
+                    "Visa",
+                    "PanAm",
+                ],
+            },
+        },
+    };
 
-    handleClient = (client_selected) => {
-        const client_name = this.state.data[client_selected].name;
-
+    handleClient = (client) => {
         this.setState(() => ({
-            client: client_name,
-            client_selected,
+            client,
             disableClient: true,
-            disableShell: false
+            disableShell: false,
         }));
-    }
+    };
 
     handleShell = (shell) => {
         this.setState(() => ({
             shell,
-            disableShell: true
+            disableShell: true,
+            disableMod: false
         }));
+    };
+
+    handleMod = (mod) => {
+        console.log(mod)
     }
 
     render() {
@@ -56,25 +90,61 @@ class App extends Component {
                                 title="Select Client"
                                 disabled={this.state.disableClient}
                             >
-                                <Dropdown.Item onClick={() => this.handleClient("ATT")}>
+                                <Dropdown.Item
+                                    onClick={() => this.handleClient("AT&T")}
+                                >
                                     AT&T
                                 </Dropdown.Item>
-                                <Dropdown.Item onClick={() => this.handleClient("HD")}>
-                                    Harley Davidson
+                                <Dropdown.Item
+                                    onClick={() => this.handleClient("H-D")}
+                                >
+                                    H-D
                                 </Dropdown.Item>
                             </DropdownButton>
 
-                            <DropdownButton
-                                id="dropdown-basic-button"
-                                title="Select Shell"
-                                disabled={this.state.disableShell}
-                            >
-                                {this.state.data[this.state.client_selected].shells.map((shell) => {
-                                    return <Dropdown.Item onClick={() => this.handleShell(shell)} key={shell}>
-                                        {shell}
-                                    </Dropdown.Item>
-                                })}
-                            </DropdownButton>
+                            {this.state.client !== "" && (
+                                <DropdownButton
+                                    id="dropdown-basic-button"
+                                    title="Select Shell"
+                                    disabled={this.state.disableShell}
+                                >
+                                    {this.state.data[
+                                        this.state.client
+                                    ].shells.map((shell) => {
+                                        return (
+                                            <Dropdown.Item
+                                                onClick={() =>
+                                                    this.handleShell(shell)
+                                                }
+                                                key={shell}
+                                            >
+                                                {shell}
+                                            </Dropdown.Item>
+                                        );
+                                    })}
+                                </DropdownButton>
+                            )}
+
+                            {this.state.shell !== "" && (
+                                <DropdownButton
+                                    id="dropdown-basic-button"
+                                    title="Select Mod"
+                                    disabled={this.state.disableMod}
+                                >
+                                    {this.state.data[this.state.client][this.state.shell].mods.map((mod) => {
+                                        return (
+                                            <Dropdown.Item
+                                                onClick={() =>
+                                                    this.handleMod(mod)
+                                                }
+                                                key={mod}
+                                            >
+                                                {mod}
+                                            </Dropdown.Item>
+                                        );
+                                    })}
+                                </DropdownButton>
+                            )}
                         </Col>
                     </Row>
                 </Container>
