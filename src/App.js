870e824a1c08af7e4959d38bcfda562bee5e01df
Dustin Wurtz <dtwurtz@gmail.com>
2021-08-28 11:38:56 -0700

    Implemented shell dropdown

--- src/App.js
+++ src/App.js
@@ -4,26 +4,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
     state = {
+        client_selected: "ATT",
         client: "",
         shell: "",
         disableClient: false,
-        disableShell: false,
+        disableShell: true,
         data: {
             ATT: {
-                name: "AT&T"
+                name: "AT&T",
+                shells: ["CLM", "CrossSell_Template", "DirectTVStream"]
             },
             HD: {
-                name: "Harly Davidson"
+                name: "Harley Davidson",
+                shells: ["eComm", "Insurance", "Museum_Rublings", "New_Brand", "Visa", "PanAm"]
             }
         }
     }
 
-    handleClient = (client) => {
-        const client_name = this.state.data[client].name;
+    handleClient = (client_selected) => {
+        const client_name = this.state.data[client_selected].name;
 
         this.setState(() => ({
             client: client_name,
-            disableClient: true
+            client_selected,
+            disableClient: true,
+            disableShell: false
+        }));
+    }
+
+    handleShell = (shell) => {
+        this.setState(() => ({
+            shell,
+            disableShell: true
         }));
     }
 
@@ -48,8 +60,20 @@ class App extends Component {
                                     AT&T
                                 </Dropdown.Item>
                                 <Dropdown.Item onClick={() => this.handleClient("HD")}>
-                                    H-D
+                                    Harley Davidson
                                 </Dropdown.Item>
+                            </DropdownButton>
+
+                            <DropdownButton
+                                id="dropdown-basic-button"
+                                title="Select Shell"
+                                disabled={this.state.disableShell}
+                            >
+                                {this.state.data[this.state.client_selected].shells.map((shell) => {
+                                    return <Dropdown.Item onClick={() => this.handleShell(shell)} key={shell}>
+                                        {shell}
+                                    </Dropdown.Item>
+                                })}
                             </DropdownButton>
                         </Col>
                     </Row>
