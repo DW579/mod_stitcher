9aa300b0f250d9115f85167b64adca0429b415e9
Dustin Wurtz <dtwurtz@gmail.com>
2021-08-28 10:39:58 -0700

    Select client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { Container, Row, Col, Dropdown, DropdownButton } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    state = {
+        client: "",
+        shell: "",
+        disableClient: false,
+        disableShell: false
+    }
+
+    handleClient = (client) => {
+        this.setState(() => ({
+            client,
+            disableClient: true
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h3>Selected client: {this.state.client}</h3>
+                            <h3>Selected shell: {this.state.shell}</h3>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DropdownButton
+                                id="dropdown-basic-button"
+                                title="Select Client"
+                                disabled={this.state.disableClient}
+                            >
+                                <Dropdown.Item onClick={() => this.handleClient("AT&T")}>
+                                    AT&T
+                                </Dropdown.Item>
+                                <Dropdown.Item onClick={() => this.handleClient("H-D")}>
+                                    H-D
+                                </Dropdown.Item>
+                            </DropdownButton>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
